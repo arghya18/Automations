bbb2e585c6b426df4b713678a4f2721e59a1e11f
arghya18 <saharghya@gmail.com>
2021-01-29 23:43:25 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -10,5 +10,5 @@ spark.sparkContext.setLogLevel("DEBUG")
 df = spark.range(10)
 df.show()
 
-path = 's3://stx-usw2-ehc-prd-staging-2/spark-k8s'
+path = 's3a://stx-usw2-ehc-prd-staging-2/spark-k8s'
 df.write.format('orc').mode('append').save(path)
