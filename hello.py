5e19f0ee761c122691e74c429055a0150c6b9e95
arghya18 <saharghya@gmail.com>
2021-01-30 00:08:20 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ warehouse_location = "abspath('/tmp/warehouse/test/')"
 spark = SparkSession.builder.appName("bad_files")\
     .config("spark.sql.warehouse.dir", warehouse_location)\
     .config("spark.hadoop.orc.overwrite.output.file", "true")\
+    .enableHiveSupport()\
     .getOrCreate()
 spark.sparkContext.setLogLevel("INFO")
 
@@ -12,3 +13,6 @@ df.show()
 
 path = 's3a://stx-usw2-ehc-prd-staging-2/spark-k8s'
 df.write.format('orc').mode('append').save(path)
+
+df2 = spark.sql('select * from dim.date_dim limit 100')
+df2.show()
