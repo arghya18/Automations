7220d5862cda32c3edbf2d246bdc43a0b6938646
arghya18 <saharghya@gmail.com>
2021-01-29 23:56:25 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ spark = SparkSession.builder.appName("bad_files")\
     .config("spark.sql.warehouse.dir", warehouse_location)\
     .config("spark.hadoop.orc.overwrite.output.file", "true")\
     .getOrCreate()
-spark.sparkContext.setLogLevel("DEBUG")
+spark.sparkContext.setLogLevel("INFO")
 
 df = spark.range(10)
 df.show()
