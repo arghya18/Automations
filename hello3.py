988d343b7790be866c6d8bb980dfef3cdc5bf6c0
arghya18 <saharghya@gmail.com>
2021-02-05 19:07:10 +0530

    Create hello3.py

---
+++ hello3.py
@@ -0,0 +1,24 @@
+import time
+from pyspark.sql import SparkSession
+
+warehouse_location = "abspath('/tmp/warehouse/test/')"
+spark = SparkSession.builder.appName("bad_files")\
+    .config("spark.sql.warehouse.dir", warehouse_location)\
+    .config("spark.hadoop.orc.overwrite.output.file", "true")\
+    .enableHiveSupport()\
+    .getOrCreate()
+spark.sparkContext.setLogLevel("ERROR")
+
+path = 's3a://stx-usw2-ehc-prd-staging-2/spark-k8s-data'
+
+df = spark.range(10)
+df.show()
+
+df2 = spark.sql('select count(*) from dim.date_dim limit 100')
+df2.show()
+df2.write.format('orc').mode('append').save(path)
+
+print('Sleeping for debug')
+time.sleep(300)
+
+df.write.format('orc').mode('append').save(path)
