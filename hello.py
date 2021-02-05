8c2914cb20f41a7e0933426968f8c124b3b3ea8c
arghya18 <saharghya@gmail.com>
2021-02-05 21:32:32 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -14,12 +14,12 @@ path = 's3a://stx-usw2-ehc-prd-staging-2/spark-k8s-data'
 df = spark.range(10)
 df.show()
 
-df2 = spark.sql('select count(*) from tmp.test_date_dim limit 100')
+df2 = spark.sql('select count(*) from dim.date_dim limit 100')
 df2.show()
 df2.write.format('orc').mode('append').save(path)
 
 print('Sleeping for debug')
-time.sleep(300)
+time.sleep(3)
 
 df.write.format('orc').mode('append').save(path)
 
