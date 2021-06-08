e8f7389319b6da3231a077a5b47d02ed165d3bc0
arghya18 <saharghya@gmail.com>
2021-06-08 14:07:39 +0530

    Update hello3.py

--- hello3.py
+++ hello3.py
@@ -1,5 +1,6 @@
 import time
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as f
 
 warehouse_location = "abspath('/tmp/warehouse/test/')"
 spark = SparkSession.builder.appName("bad_files")\
@@ -9,16 +10,12 @@ spark = SparkSession.builder.appName("bad_files")\
     .getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 
-path = 's3a://stx-usw2-ehc-prd-staging-2/spark-k8s-data'
+dad_df = spark.sql("select serial_num, event_date, trans_seq from dim.drive_attr_dim dad where dad.hadoop_etl_load_date > '2021-06-07 01:45:40' and dad.hadoop_etl_load_date <= '2021-06-08 07:12:35' and dad.event_date_key in (20210604, 20210527, 20210508, 20210607, 20210603, 20210608, 20201110, 20210606, 20210605)")
+dad_df.createOrReplaceTempView("drive_attr_delta")
 
-df = spark.range(10)
-df.show()
+def_backfill = spark.sql("select drive_serial_num,drive_event_fact.trans_seq,drive_event_fact.event_date, date_key  from drive.drive_event_fact inner join drive_attr_delta on (drive_attr_delta.serial_num=drive_event_fact.drive_serial_num and (drive_event_fact.event_date > drive_attr_delta.event_date or (drive_event_fact.event_date = drive_attr_delta.event_date and drive_event_fact.trans_seq >= drive_attr_delta.trans_seq))) where date_key between date_format(date_sub(to_timestamp('2021-06-07 01:45:40'),15),'yyyyMMdd') and date_format(date_add(to_timestamp('2021-06-07 01:45:40'),1),'yyyyMMdd') and event_status in ('P', 'F' ,'PASS', 'FAIL') and drive_event_fact.hadoop_etl_load_date <= '2021-06-07 01:45:40'")
 
-df2 = spark.read.format('orc').option('compression', 'zlib').load('s3://stx-usw2-ehc-prd-data-t2/dim.db_date_dim/atlas_insert_ver=2102050716')
-df2.show()
+def_backfill.explain()
 
-
-print('Sleeping for debug')
-time.sleep(300)
-
-df.write.format('orc').mode('append').save(path)
+union_partitions = def_backfill.select(f.col("date_key")).distinct().rdd.flatMap(lambda x: x).collect()
+print(union_partitions)
