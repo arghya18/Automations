f76844c74e761bce5298462de682f67562e25054
arghya18 <saharghya@gmail.com>
2021-06-09 16:12:53 +0530

    Update hello3.py

--- hello3.py
+++ hello3.py
@@ -8,7 +8,7 @@ spark = SparkSession.builder.appName("bad_files")\
     .config("spark.hadoop.orc.overwrite.output.file", "true")\
     .enableHiveSupport()\
     .getOrCreate()
-spark.sparkContext.setLogLevel("DEBUG")
+spark.sparkContext.setLogLevel("ERROR")
 
 """
 dad_df = spark.sql("select serial_num, event_date, trans_seq from dim.drive_attr_dim dad where dad.hadoop_etl_load_date > '2021-06-07 01:45:40' and dad.hadoop_etl_load_date <= '2021-06-08 07:12:35' and dad.event_date_key in (20210604, 20210527, 20210508, 20210607, 20210603, 20210608, 20201110, 20210606, 20210605)")
@@ -20,11 +20,11 @@ def_backfill.explain()
 
 union_partitions = def_backfill.select(f.col("date_key")).distinct().rdd.flatMap(lambda x: x).collect()
 print(union_partitions)
-and date_key is not null and insert_version is not null
+date_key=20201204 and insert_version=2012210729 and date_key is not null and insert_version is not null
 """
 df = spark.read.format("orc").option("compression", "zlib").load("s3://stx-usw2-ehc-prd-data-t2/dim_flat.db_drive_event_attr_flat/date_key=20201204/insert_version=2012210729/part-00001-5e22a873-82a5-4781-9eb9-473b483396bd.c000.zlib.orc")
 df.createOrReplaceTempView("drive_event_attr_flat")
-df2 = spark.sql("select * from drive_event_attr_flat where date_key=20201204 and insert_version=2012210729 and event_date is not null and drive_serial_num is not null")
+df2 = spark.sql("select * from drive_event_attr_flat where event_date is not null and drive_serial_num is not null")
 df2.write.format("orc").option("compression", "zlib").mode("Append").save("s3a://stx-usw2-ehc-prd-staging-2/spark-k8s-test2")
 
 spark.stop()
