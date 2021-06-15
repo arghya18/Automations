53c872daeae47657b966beadea3bc32e725ba89a
arghya18 <saharghya@gmail.com>
2021-06-15 16:54:59 +0530

    Create hello4.py

---
+++ hello4.py
@@ -0,0 +1,27 @@
+import time
+import pymysql
+from pyspark.sql import SparkSession
+import sys
+import json
+
+warehouse_location = "abspath('/tmp/warehouse/test/')"
+spark = SparkSession.builder.appName("bad_files")\
+    .config("spark.sql.warehouse.dir", warehouse_location)\
+    .config("spark.hadoop.orc.overwrite.output.file", "true")\
+    .enableHiveSupport()\
+    .getOrCreate()
+spark.sparkContext.setLogLevel("INFO")
+
+print(sys.argv[1])
+args = json.loads(sys.argv[1])
+print(args)
+
+df = spark.range(10)
+df.show()
+
+df2 = spark.sql('show create table lyve_lab.date_dim')
+df2.show()
+print('Sleeping for debug')
+time.sleep(300)
+
+spark.stop()
