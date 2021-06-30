9f1765555b387d1cacc43ec944d0ce77b9374c0f
arghya18 <saharghya@gmail.com>
2021-07-01 02:02:41 +0530

    Create hello6.py

---
+++ hello6.py
@@ -0,0 +1,22 @@
+import time
+import pymysql
+from pyspark.sql import SparkSession
+import sys
+import json
+
+warehouse_location = "abspath('/tmp/warehouse/test/')"
+spark = SparkSession.builder.appName("bad_files").config("spark.sql.warehouse.dir", warehouse_location).getOrCreate()
+spark.sparkContext.setLogLevel("INFO")
+
+print(sys.argv[1])
+args = json.loads(sys.argv[1])
+print(args)
+
+df = spark.range(10)
+df.show()
+
+df1 = spark.read.format("orc").option("compression", "zlib").load("s3a://stx-usw2-ehc-prd-data-t1/dim.db_media_sbr_lkp/20210607215800-20210608215700")
+df1.show(100)
+time.sleep(300)
+
+spark.stop()
