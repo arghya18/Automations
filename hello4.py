2da5e9365bea3240f7187516692a482cbe6abe02
arghya18 <saharghya@gmail.com>
2021-07-01 01:58:45 +0530

    Update hello4.py

--- hello4.py
+++ hello4.py
@@ -5,10 +5,7 @@ import sys
 import json
 
 warehouse_location = "abspath('/tmp/warehouse/test/')"
-spark = SparkSession.builder.appName("bad_files")\
-    .config("spark.sql.warehouse.dir", warehouse_location)\
-    .config("spark.hadoop.orc.overwrite.output.file", "true")\
-    .getOrCreate()
+spark = SparkSession.builder.appName("bad_files").config("spark.sql.warehouse.dir", warehouse_location).getOrCreate()
 spark.sparkContext.setLogLevel("INFO")
 
 print(sys.argv[1])
